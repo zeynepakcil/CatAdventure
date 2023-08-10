eae46b104ad97094a866c0704988a5546c4c99f0
zeynepakcil <52704160+zeynepakcil@users.noreply.github.com>
2023-08-11 00:24:00 +0300

    Add files via upload

---
+++ assets/level_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="level_tileset" tilewidth="16" tileheight="16" tilecount="324" columns="18">
+ <image source="level_tileset.png" width="300" height="300"/>
+</tileset>
---
+++ assets/maps/tiles/nature-paltformer-tileset-16x16 - Copy.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="nature-paltformer-tileset-16x16 - Copy" tilewidth="16" tileheight="16" tilecount="77" columns="7">
+ <image source="nature-paltformer-tileset-16x16 - Copy.png" width="112" height="176"/>
+</tileset>
