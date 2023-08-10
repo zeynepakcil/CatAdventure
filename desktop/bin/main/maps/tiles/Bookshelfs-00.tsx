03b5331f55d9a94ef2cdb4473773cacad6e3c8d0
zeynepakcil <52704160+zeynepakcil@users.noreply.github.com>
2023-08-11 00:28:33 +0300

    Add files via upload

---
+++ desktop/bin/main/maps/tiles/Bookshelfs-00.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Bookshelfs-00" tilewidth="16" tileheight="16" tilecount="1280" columns="40">
+ <image source="Bookshelfs-00.png" width="640" height="512"/>
+</tileset>
---
+++ desktop/bin/main/maps/tiles/pixel-art-style-set-of-different-texture-vector-23823354.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="pixel-art-style-set-of-different-texture-vector-23823354" tilewidth="16" tileheight="16" tilecount="3844" columns="62">
+ <image source="pixel-art-style-set-of-different-texture-vector-23823354.jpg" width="1000" height="1002"/>
+</tileset>
