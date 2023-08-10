ea85596afa5d4e3169137802a843c20336fb7a63
zeynepakcil <52704160+zeynepakcil@users.noreply.github.com>
2023-08-11 00:30:54 +0300

    Add files via upload

---
+++ desktop/src/DesktopLauncher.java
@@ -0,0 +1,16 @@
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.mygdx.demo.TuruncsAdventure;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowedMode(1200, 610);
+		config.setForegroundFPS(60);
+		config.setTitle("Turunc's Adventure");
+		config.setResizable(false);
+		new Lwjgl3Application(new TuruncsAdventure(), config);
+	}
+}
